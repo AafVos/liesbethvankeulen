f8fa779e309d1e0bcc1179e965a2dc0eab4ea710
a.vos <a.vos@in2intel.com>
2025-04-18 20:44:02 +0200

    Fix caching issues by adding no-store headers and force rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextVideos from 'next-videos';
+
+const nextConfig = {
+  ...nextVideos(),
+  images: {
+    domains: ['images.ctfassets.net'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
